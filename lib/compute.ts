8343bf92fbfc2eecc252466f104986a92cefcf1c
ito.toshifumi <itotoshifumi@airtrip-corp.com>
2020-09-17 04:41:58 +0900

    typo

--- lib/compute.ts
+++ lib/compute.ts
@@ -7,21 +7,35 @@ interface ComputeProps {
 }
 
 export class Compute extends cdk.Construct {
-  public readonly instances: { [key: string]: ec2.Instance } = {};
+  public readonly instances: { [key: string]: ec2.Instance | ec2.BastionHostLinux } = {};
   public readonly asgs:      { [key: string]: asg.AutoScalingGroup } = {};
 
   constructor(parent: cdk.Construct, name: string, props: ComputeProps) {
     super(parent, name);
 
     const env:      string  = this.node.tryGetContext('env');
-    const private_subnet    = { subnetType: ec2.SubnetType.PRIVATE }
-    const public_subnet     = { subnetType: ec2.SubnetType.PUBLIC }
+
+    // subnets
+    const public_subnet:         ec2.SubnetSelection = { subnetGroupName: `${env}-public` }
+    const private_subnet:        ec2.SubnetSelection = { subnetGroupName: `${env}-private` }
+    const private_secure_subnet: ec2.SubnetSelection = { subnetGroupName: `${env}-private-secure` }
+
+    // ami
+    const amazonlinux2 = new ec2.AmazonLinuxImage({ generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2 })
+    const bastion      = new ec2.LookupMachineImage({ name: "bastion" })
+
+    // setup commands
+    const setup_comand = [
+      "sudo timedatectl set-timezone Asia/Tokyo",
+      "sudo yum update -y",
+      "sudo yum install -y vim git util-linux-user",
+    ]
 
     this.instances["bastion"] = new ec2.Instance(this, `${env}-bastion`, {
       vpc: props.vpc,
       vpcSubnets: public_subnet,
       instanceType: new ec2.InstanceType("t3a.micro"),
-      machineImage: new ec2.LookupMachineImage({ name: "bastion" }),
+      machineImage: bastion,
       keyName: this.node.tryGetContext('bastion-keyname')
     })
 
@@ -30,7 +44,7 @@ export class Compute extends cdk.Construct {
       vpc: props.vpc,
       vpcSubnets: private_subnet,
       instanceType: new ec2.InstanceType("t3a.micro"),
-      machineImage: new ec2.AmazonLinuxImage({ generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2 }),
+      machineImage: amazonlinux2,
       keyName: "bastion"
     })
 
@@ -38,26 +52,27 @@ export class Compute extends cdk.Construct {
       vpc: props.vpc,
       vpcSubnets: private_subnet,
       instanceType: new ec2.InstanceType("t3a.micro"),
-      machineImage: new ec2.AmazonLinuxImage({ generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2 }),
+      machineImage: amazonlinux2,
       keyName: "bastion"
     })
 
     // default setup
     for (let [name, node] of Object.entries(this.instances)) {
-      // execute comannds
-      node.addUserData(
-        `hostnamectl set-hostname ${env}-${name}`,
-        "sudo yum update -y",
-        "sudo yum install -y vim git zsh util-linux-user",
-        "sudo yum install -y gcc openssl-devel readline-devel zlib-devel"
-      )
-
-      if (name == "bastion") {
-        node.connections.allowFrom(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'allow ssh connection for bastion')
-      } else {
+      if (node instanceof ec2.Instance) {
+        node.addUserData(
+          `hostnamectl set-hostname ${env}-${name}`,
+          ...setup_comand
+        )
         node.connections.allowFrom(this.instances["bastion"].connections, ec2.Port.tcp(22), 'allow ssh connection for bastion')
-      }
-      node.connections.allowFrom(ec2.Peer.anyIpv4(), ec2.Port.icmpPing(), 'allow icmp')
+        node.connections.allowFrom(ec2.Peer.anyIpv4(), ec2.Port.icmpPing(), 'allow icmp')
+      } else if (node instanceof ec2.BastionHostLinux) {
+        node.instance.addUserData(
+          `hostnamectl set-hostname ${env}-${name}`,
+          ...setup_comand
+        )
+
+        node.allowSshAccessFrom(ec2.Peer.anyIpv4())
+      };
     };
 
     for (let [name, asg] of Object.entries(this.asgs)) {
