fef8d2115c717969ec727f4b8dfb627fb14b3aa6
ito.toshifumi <itotoshifumi@airtrip-corp.com>
2020-09-17 00:23:43 +0900

    refactoring

--- lib/network.ts
+++ lib/network.ts
@@ -1,28 +1,26 @@
-import { Construct } from "@aws-cdk/core";
-import {
-  Vpc,
-  SubnetType,
-} from "@aws-cdk/aws-ec2";
 
-export class Network extends Construct {
-  public readonly vpc: Vpc;
+import cdk   = require("@aws-cdk/core");
+import ec2   = require("@aws-cdk/aws-ec2");
+
+export class Network extends cdk.Construct {
+  public readonly vpc: ec2.Vpc;
 
   //public readonly dbSg: SecurityGroup;
-  constructor(parent: Construct, name: string) {
+  constructor(parent: cdk.Construct, name: string) {
     super(parent, name);
 
     const env: string = this.node.tryGetContext('env');
     const params: any = this.node.tryGetContext(env);
 
     // create vpc
-    this.vpc = new Vpc(this, `${env}-vpc`, {
+    this.vpc = new ec2.Vpc(this, `${env}-vpc`, {
       cidr: params["cibr"],
       enableDnsHostnames: true,
       enableDnsSupport: true,
       maxAzs: params["maxAzs"],
       subnetConfiguration: [
-        { name: `${env}-private`, subnetType: SubnetType.PRIVATE, cidrMask: 24 },
-        { name: `${env}-public`, subnetType: SubnetType.PUBLIC, cidrMask: 24 }
+        { name: `${env}-private`, subnetType: ec2.SubnetType.PRIVATE, cidrMask: 24 },
+        { name: `${env}-public`,  subnetType: ec2.SubnetType.PUBLIC,  cidrMask: 24 }
       ]
     });
     this.vpc.addFlowLog(`${env}-vpc-flow-log`)
